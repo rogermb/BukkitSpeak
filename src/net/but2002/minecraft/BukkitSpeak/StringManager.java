a36e87bb104cadace8433d74025890a9f76a5059
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2012-03-03 00:01:16 +0100

    Added a ConfigReader, what should improve the code at StringReader.java as well. No more copy-paste code

--- src/net/but2002/minecraft/BukkitSpeak/StringManager.java
+++ src/net/but2002/minecraft/BukkitSpeak/StringManager.java
@@ -2,10 +2,11 @@ package net.but2002.minecraft.BukkitSpeak;
 
 import java.util.HashMap;
 
-import org.bukkit.configuration.file.FileConfiguration;
+import net.but2002.minecraft.BukkitSpeak.util.ConfigReader;
 
 public class StringManager {
 	
+	private ConfigReader reader;
 	private HashMap<String,String> strings = new HashMap<String,String>();
 	private String ip, serverAdmin, serverPass;
 	private int queryPort,serverPort;
@@ -16,125 +17,39 @@ public class StringManager {
 	public static final String CONFIG_QUERYPORT = "QueryPort";
 	public static final String CONFIG_SERVERADMIN = "QueryUsername";
 	public static final String CONFIG_SERVERPASS = "QueryPassword";
-
-	public static final String MESSAGES_SECTION = "Messages";
-	public static final String[] MESSAGES = {"msg_join", "msg_quit", "msg_servermsg", "msg_list"};
-
-	public static final String MUTED_SECTION = "Muted";
 	
-	private BukkitSpeak plugin;
+	public static final String MESSAGES_SECTION = "Messages";
+	public static final String[][] MESSAGES = {
+		{"msg_join", "%name% has joined TeamSpeak"}, 
+		{"msg_quit", "%name% has left TeamSpeak"}, 
+		{"msg_servermsg", "[TS][%client_nickname%] %msg%"}, 
+		{"msg_list", "Currently online: "}};
+	
+	public static final String MUTED_SECTION = "Muted";
 	
 	public StringManager(BukkitSpeak plugin) {
 		
-		this.plugin = plugin;
-		FileConfiguration config = plugin.getConfig();
-		boolean error = false;
+		reader = new ConfigReader(plugin);
 		
-		if (config.getValues(true).isEmpty()) {
-			setDefaults(config);
-			plugin.saveConfig();
-		}
+		ip = reader.getString(CONFIG_SECTION, CONFIG_IP, "1.2.3.4");
+		serverPort = reader.getInteger(CONFIG_SECTION, CONFIG_SERVERPORT, 9987);
+		queryPort = reader.getInteger(CONFIG_SECTION, CONFIG_QUERYPORT, 10011);
+		serverAdmin = reader.getString(CONFIG_SECTION, CONFIG_SERVERADMIN, "admin");
+		serverPass = reader.getString(CONFIG_SECTION, CONFIG_SERVERPASS, "123456");
 		
-		try {
-			config.getConfigurationSection(CONFIG_SECTION);
-		} catch(Exception e) {
-			config.createSection(CONFIG_SECTION);
-		}
-		
-		try {
-			config.getConfigurationSection(MESSAGES_SECTION);
-		} catch(Exception e) {
-			config.createSection(MESSAGES_SECTION);
-		}
-
-		try {
-			ip = config.getConfigurationSection(CONFIG_SECTION).getString(CONFIG_IP);
-		} catch(Exception e) {
-			config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_IP, getDefault(CONFIG_IP));
-			logConfigError(CONFIG_IP);
-			error = true;
-		}
-		
-		try {
-			serverPort = Integer.parseInt(config.getConfigurationSection(CONFIG_SECTION).getString(CONFIG_SERVERPORT));
-		} catch (Exception e) {
-			config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_SERVERPORT, getDefault(CONFIG_SERVERPORT));
-			logConfigError(CONFIG_SERVERPORT);
-			error = true;
-		}
-
-		try {
-			queryPort = Integer.parseInt(config.getConfigurationSection(CONFIG_SECTION).getString(CONFIG_QUERYPORT));
-		} catch (Exception e) {
-			config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_QUERYPORT, getDefault(CONFIG_QUERYPORT));
-			logConfigError(CONFIG_QUERYPORT);
-			error = true;
-		}
-		
-		try {
-			serverAdmin = config.getConfigurationSection(CONFIG_SECTION).getString(CONFIG_SERVERADMIN);
-		} catch (Exception e) {
-			config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_SERVERADMIN, getDefault(CONFIG_SERVERADMIN));
-			logConfigError(CONFIG_SERVERADMIN);
-			error = true;
-		}
-                
-		try {
-			serverAdmin = config.getConfigurationSection(CONFIG_SECTION).getString(CONFIG_SERVERPASS);
-		} catch (Exception e) {
-			config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_SERVERPASS, getDefault(CONFIG_SERVERPASS));
-			logConfigError(CONFIG_SERVERPASS);
-			error = true;
-		}
-		
-		
-		for (String currentNode : MESSAGES) { 
-			String currentValue = config.getConfigurationSection(MESSAGES_SECTION).getString(currentNode);
-			if (currentValue == null) {
-				config.getConfigurationSection(MESSAGES_SECTION).set(currentNode, getDefault(currentNode));
-				logConfigError(currentNode);
-				error = true;
-			} else {
-				strings.put(currentNode, currentValue);
+		for (String[] keyPair : MESSAGES) {
+			try {
+				String currentValue = reader.getString(MESSAGES_SECTION, keyPair[0], keyPair[1]);
+				strings.put(keyPair[1], currentValue);
+			} catch (Exception e) {
+				plugin.getLogger().severe(plugin + "was unable to load all the messages. This is probably a programming error.");
 			}
 		}
 		
-		if(error){
-			plugin.saveConfig();
-			plugin.disable();
-		}
+		plugin.saveConfig();
 		
 	}
-	
-	private void logConfigError(String message){
-		plugin.getLogger().severe(plugin + "Error while parsing " + message);
-	}
-	
-	private void setDefaults(FileConfiguration config) {
-		config.createSection(CONFIG_SECTION);
-		config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_IP, "1.2.3.4");
-		config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_SERVERPORT, "9987");
-		config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_QUERYPORT, "10011");
-		config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_SERVERADMIN, "ServerAdmin");
-		config.getConfigurationSection(CONFIG_SECTION).set(CONFIG_SERVERPASS, "ServerPass");
-		config.createSection(MESSAGES_SECTION);
-		config.getConfigurationSection(MESSAGES_SECTION).set(MESSAGES[0], "%name% has joined TeamSpeak");
-		config.getConfigurationSection(MESSAGES_SECTION).set(MESSAGES[1], "%name% has left TeamSpeak");
-		config.getConfigurationSection(MESSAGES_SECTION).set(MESSAGES[2], "[TS][%client_nickname%] %msg%");
-	}
-	
-	private String getDefault(String node) {
-		if(node.equals(CONFIG_IP)) return "1.2.3.4";
-		else if (node.equals(CONFIG_SERVERPORT)) return "9987";
-		else if (node.equals(CONFIG_QUERYPORT)) return "10011";
-		else if (node.equals(CONFIG_SERVERADMIN)) return "ServerAdmin";
-		else if (node.equals(CONFIG_SERVERPASS)) return "ServerPass";
-		else if (node.equals(MESSAGES[0])) return "%name% has joined TeamSpeak";
-		else if (node.equals(MESSAGES[1])) return "%name% has quit TeamSpeak";
-		else if (node.equals(MESSAGES[2])) return "[TS][%client_nickname%] %msg%";
-		else return "NO_DEFAULT_SET";
-	}
-	
+		
 	public String getMessage(String key){
 		return strings.get(key);
 	}
@@ -150,13 +65,12 @@ public class StringManager {
 	public int getServerPort() {
 		return serverPort;
 	}
-    
+	
 	public String getServerAdmin() {
-        return serverAdmin;
-    }
-    
-    public String getServerPass() {
-        return serverPass;
-    }
-
+		return serverAdmin;
+	}
+	
+	public String getServerPass() {
+		return serverPass;
+	}
 }
---
+++ src/net/but2002/minecraft/BukkitSpeak/util/ConfigReader.java
@@ -0,0 +1,93 @@
+package net.but2002.minecraft.BukkitSpeak.util;
+
+import java.util.HashMap;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import net.but2002.minecraft.BukkitSpeak.BukkitSpeak;
+
+public class ConfigReader {
+	
+	BukkitSpeak plugin;
+	FileConfiguration config;
+	
+	public ConfigReader(BukkitSpeak BukkitSpeak) {
+		plugin = BukkitSpeak;
+		config = BukkitSpeak.getConfig();
+	}
+	
+	public HashMap<String, ?> getAll(HashMap<String, ?> map) {
+		Object def;
+		String[] loc;
+		for (String key : map.keySet()) {
+			try {
+				def = map.get(key);
+				loc = key.split(".");
+				if (def.getClass() == Boolean.class) {
+					getBoolean(loc[0], loc[1], (Boolean) def);
+				} else if (def.getClass() == Integer.class) {
+					getInteger(loc[0], loc[1], (Integer) def);
+				} else if (def.getClass() == String.class) {
+					getString(loc[0], loc[1], (String) def);
+				} else {
+					logConfigError(key);
+				}
+			} catch (Exception e) {
+				logConfigError(key);
+			}
+			
+		}
+		def = null;
+		loc = null;
+		return map;
+	}
+	
+	public Boolean getBoolean(String dir, String loc, Boolean def) {
+		EnsureSectionCreated(dir);
+		
+		try {
+			return config.getConfigurationSection(dir).getBoolean(loc);
+		} catch (Exception e) {
+			config.getConfigurationSection(dir).set(loc, def);
+			logConfigError(dir + "." + loc);
+			return def;
+		}
+	}
+	
+	public Integer getInteger(String dir, String loc, Integer def) {
+		EnsureSectionCreated(dir);
+		
+		try {
+			return config.getConfigurationSection(dir).getInt(loc);
+		} catch (Exception e) {
+			config.getConfigurationSection(dir).set(loc, def);
+			logConfigError(dir + "." + loc);
+			return def;
+		}
+	}
+	
+	public String getString(String dir, String loc, String def) {
+		EnsureSectionCreated(dir);
+		
+		try {
+			return config.getConfigurationSection(dir).getString(loc);
+		} catch (Exception e) {
+			config.getConfigurationSection(dir).set(loc, def);
+			logConfigError(dir + "." + loc);
+			return def;
+		}
+	}
+	
+	public void EnsureSectionCreated(String dir) {
+		try {
+			config.getConfigurationSection(dir);
+		} catch(Exception e) {
+			config.createSection(dir);
+		}
+	}
+	
+	private void logConfigError(String message){
+		plugin.getLogger().severe(plugin + "Error while parsing " + message);
+	}
+	
+}

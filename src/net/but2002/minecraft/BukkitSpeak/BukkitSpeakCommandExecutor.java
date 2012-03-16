62b14f97445aaabae361533194a3647bd554c691
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2012-03-16 08:09:45 +0100

    Only made broadcast and chat work if UseTextServer or UseTextChannel are enabled.

--- src/net/but2002/minecraft/BukkitSpeak/BukkitSpeakCommandExecutor.java
+++ src/net/but2002/minecraft/BukkitSpeak/BukkitSpeakCommandExecutor.java
@@ -23,6 +23,7 @@ public class BukkitSpeakCommandExecutor implements CommandExecutor {
 			send(sender, Level.INFO, "&aHelp");
 			if (CheckPermissions(sender, "list")) send(sender, Level.INFO, "&e/ts list &a- Displays a list of people who are currently online");
 			if (CheckPermissions(sender, "mute")) send(sender, Level.INFO, "&e/ts mute &a- Mutes / unmutes BukkitSpeak for you");
+			if (CheckPermissions(sender, "chat") && plugin.getStringManager().getUseTextChannel()) send(sender, Level.INFO, "&e/ts chat &a- Displays a message only in the TS channel.");
 			if (CheckPermissions(sender, "broadcast")) send(sender, Level.INFO, "&e/ts broadcast &a- Broadcasts a global message in TeamSpeak.");
 			if (CheckPermissions(sender, "reload")) send(sender, Level.INFO, "&e/ts reload &a- Reloads the BukkitSpeak config and restarts the TS listener");
 			return true;
@@ -38,9 +39,17 @@ public class BukkitSpeakCommandExecutor implements CommandExecutor {
 			if (!CheckPermissions(sender, "mute")) return false;
 			Mute(sender, args);
 		} else if (args[0].equalsIgnoreCase("chat")) {
+			if (!plugin.getStringManager().getUseTextChannel()) {
+				send(sender, Level.INFO, "You need to enable ListenToServerBroadcasts in the config to use this command.");
+				return true;
+			}
 			if (!CheckPermissions(sender, "chat")) return false;
 			Chat(sender, args);
 		} else if (args[0].equalsIgnoreCase("broadcast")) {
+			if (!plugin.getStringManager().getUseTextServer()) {
+				send(sender, Level.INFO, "You need to enable ListenToChannelChat in the config to use this command.");
+				return true;
+			}
 			if (!CheckPermissions(sender, "broadcast")) return false;
 			Broadcast(sender, args);
 		} else if (args[0].equalsIgnoreCase("reload")) {

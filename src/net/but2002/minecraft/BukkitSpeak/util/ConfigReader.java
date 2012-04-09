1417fdcf8cd33a6518210eb20806fa409bbae031
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2012-04-09 02:18:56 +0200

    Improved the ConfigReader. New functions: - Multiple config sections - Get the index of a collection of possible settings.

--- src/net/but2002/minecraft/BukkitSpeak/util/ConfigReader.java
+++ src/net/but2002/minecraft/BukkitSpeak/util/ConfigReader.java
@@ -2,6 +2,7 @@ package net.but2002.minecraft.BukkitSpeak.util;
 
 import java.util.HashMap;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -52,6 +53,22 @@ public class ConfigReader {
 			return def;
 		}
 	}
+	public Boolean getBoolean(String[] dir, String loc, Boolean def) {
+		EnsureSectionCreated(dir);
+		
+		ConfigurationSection cs = config;
+		for (String d : dir) {
+			cs = cs.getConfigurationSection(d);
+		}
+		
+		if (cs.isBoolean(loc)) {
+			return cs.getBoolean(loc);
+		} else {
+			cs.set(loc, def);
+			logConfigError(getAbsolutePath(dir) + "." + loc);
+			return def;
+		}
+	}
 	
 	public Integer getInteger(String dir, String loc, Integer def) {
 		EnsureSectionCreated(dir);
@@ -61,6 +78,22 @@ public class ConfigReader {
 		} else {
 			config.getConfigurationSection(dir).set(loc, def);
 			logConfigError(dir + "." + loc);
+			return def;
+		}
+	}
+	public Integer getInteger(String[] dir, String loc, Integer def) {
+		EnsureSectionCreated(dir);
+		
+		ConfigurationSection cs = config;
+		for (String d : dir) {
+			cs = cs.getConfigurationSection(d);
+		}
+		
+		if (cs.isInt(loc)) {
+			return cs.getInt(loc);
+		} else {
+			cs.set(loc, def);
+			logConfigError(getAbsolutePath(dir) + "." + loc);
 			return def;
 		}
 	}
@@ -76,15 +109,147 @@ public class ConfigReader {
 			return def;
 		}
 	}
+	public String getString(String[] dir, String loc, String def) {
+		EnsureSectionCreated(dir);
+		
+		ConfigurationSection cs = config;
+		for (String d : dir) {
+			cs = cs.getConfigurationSection(d);
+		}
+		
+		if (cs.isString(loc)) {
+			return cs.getString(loc);
+		} else {
+			cs.set(loc, def);
+			logConfigError(getAbsolutePath(dir) + "." + loc);
+			return def;
+		}
+	}
+	
+	public Integer getChoice(String dir, String loc, String def, HashMap<String, Integer> poss) {
+		EnsureSectionCreated(dir);
+		
+		if (config.getConfigurationSection(dir).isString(loc) && poss.containsKey(config.getConfigurationSection(dir).getString(loc))) {
+			return poss.get(config.getConfigurationSection(dir).getString(loc));
+		} else {
+			config.getConfigurationSection(dir).set(loc, def);
+			logConfigError(dir + "." + loc);
+			return poss.get(def);
+		}
+	}
+	public Integer getChoice(String[] dir, String loc, String def, HashMap<String, Integer> poss) {
+		EnsureSectionCreated(dir);
+		
+		ConfigurationSection cs = config;
+		for (String d : dir) {
+			cs = cs.getConfigurationSection(d);
+		}
+		
+		if (cs.isString(loc) && poss.containsKey(cs.getString(loc))) {
+			return poss.get(cs.getString(loc));
+		} else {
+			cs.set(loc, def);
+			logConfigError(dir + "." + loc);
+			return poss.get(def);
+		}
+	}
+	
+	public Integer getChoice(String dir, String loc, Integer def, String[][] poss) {
+		EnsureSectionCreated(dir);
+		
+		if (config.getConfigurationSection(dir).isString(loc)) {
+			String v = config.getConfigurationSection(dir).getString(loc);
+			Integer res = -1;
+			for (Integer i = 0; i < poss.length; i++) {
+				String[] p = poss[i];
+				if (ArrContains(p, v)) {
+					res = i;
+					break;
+				}
+			}
+			if (res >= 0) {
+				return res;
+			} else {
+				config.getConfigurationSection(dir).set(loc, poss[def][0]);
+				logConfigError(dir + "." + loc);
+				return def;
+			}
+		} else {
+			config.getConfigurationSection(dir).set(loc, poss[def][0]);
+			logConfigError(dir + "." + loc);
+			return def;
+		}
+	}
+	public Integer getChoice(String[] dir, String loc, Integer def, String[][] poss) {
+		EnsureSectionCreated(dir);
+		
+		ConfigurationSection cs = config;
+		for (String d : dir) {
+			cs = cs.getConfigurationSection(d);
+		}
+		
+		if (cs.isString(loc)) {
+			String v = cs.getString(loc);
+			Integer res = -1;
+			for (Integer i = 0; i < poss.length; i++) {
+				String[] p = poss[i];
+				if (ArrContains(p, v)) {
+					res = i;
+					break;
+				}
+			}
+			if (res >= 0) {
+				return res;
+			} else {
+				cs.set(loc, poss[def][0]);
+				logConfigError(dir + "." + loc);
+				return def;
+			}
+		} else {
+			cs.set(loc, poss[def][0]);
+			logConfigError(dir + "." + loc);
+			return def;
+		}
+	}
 	
 	public void EnsureSectionCreated(String dir) {
 		if (!config.isConfigurationSection(dir)) {
 			config.createSection(dir);
 		}
 	}
+	public void EnsureSectionCreated(String[] dir) {
+		ConfigurationSection cs = config;
+		for (String d : dir) {
+			if (!cs.isConfigurationSection(d)) {
+				cs.createSection(d);
+			}
+			cs = cs.getConfigurationSection(d);
+		}
+	}
+	
+	private String getAbsolutePath(String[] dir) {
+		StringBuilder sb = new StringBuilder();;
+		for (String d : dir) {
+			sb.append(d);
+			sb.append(config.options().pathSeparator());
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+	
+	private boolean ArrContains(String[] arr, String con) {
+		
+		boolean ret = false;
+		for (String a : arr) {
+			if (a.equalsIgnoreCase(con)) {
+				ret = true;
+				break;
+			}
+		}
+		return ret;
+	}
 	
 	private void logConfigError(String message){
 		plugin.getLogger().severe("Error while parsing " + message);
 	}
-	
 }

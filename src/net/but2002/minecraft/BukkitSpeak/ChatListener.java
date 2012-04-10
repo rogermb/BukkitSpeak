b0337f9ba31b54990c01cdca897dc520d6bcce22
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2012-04-10 21:35:46 +0200

    Made the ChatListener filter out all colors because they can't be displayed in TeamSpeak.

--- src/net/but2002/minecraft/BukkitSpeak/ChatListener.java
+++ src/net/but2002/minecraft/BukkitSpeak/ChatListener.java
@@ -1,5 +1,7 @@
 package net.but2002.minecraft.BukkitSpeak;
 
+import java.util.HashMap;
+
 import net.but2002.minecraft.BukkitSpeak.Commands.BukkitSpeakCommand;
 
 import org.bukkit.event.EventHandler;
@@ -22,16 +24,18 @@ public class ChatListener implements Listener {
 	public void onPlayerChat(PlayerChatEvent e) {
 		if (e.getPlayer() == null || e.getMessage().isEmpty()) return;
 		
+		String msg = e.getMessage();
+		msg = msg.replaceAll("(§([a-fk-orA-FK-OR0-9]))", "");
+		
 		if (stringManager.getTeamspeakTarget() == TsTargetEnum.CHANNEL) {
 			ts.pushMessage("sendtextmessage targetmode=2"
 					+ " target=" + stringManager.getChannelID()
-					+ " msg=" + convert(e.getMessage()), e.getPlayer().getName());
+					+ " msg=" + convert(msg), e.getPlayer().getName());
 		} else if (stringManager.getTeamspeakTarget() == TsTargetEnum.SERVER) {
 			ts.pushMessage("sendtextmessage targetmode=3"
 					+ " target=0"
-					+ " msg=" + convert(e.getMessage()), e.getPlayer().getName());
+					+ " msg=" + convert(msg), e.getPlayer().getName());
 		}
-		
 	}
 	
 	public String convert(String input) {

874ae04176011bea765f2154f933ceb6fb62dae3
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2012-10-20 17:34:32 +0200

    Changed the way required parameters are displayed. Now it's <target> or whatever.

--- src/main/java/net/but2002/minecraft/BukkitSpeak/Commands/CommandAdminHelp.java
+++ src/main/java/net/but2002/minecraft/BukkitSpeak/Commands/CommandAdminHelp.java
@@ -10,11 +10,11 @@ public class CommandAdminHelp extends BukkitSpeakCommand {
 	public void execute(CommandSender sender, String[] args) {
 		send(sender, Level.INFO, "&2Admin Commands Help");
 		if (checkCommandPermission(sender, "ban"))
-			send(sender, Level.INFO, "&e/tsa ban target (reason) &2- Bans a client.");
+			send(sender, Level.INFO, "&e/tsa ban <target> (reason) &2- Bans a client.");
 		if (checkCommandPermission(sender, "kick"))
-			send(sender, Level.INFO, "&e/tsa kick target (reason) &2- Kicks from the TS.");
+			send(sender, Level.INFO, "&e/tsa kick <target> (reason) &2- Kicks from the TS.");
 		if (checkCommandPermission(sender, "channelkick"))
-			send(sender, Level.INFO, "&e/tsa channelkick target (reason) &2- "
+			send(sender, Level.INFO, "&e/tsa channelkick <target> (reason) &2- "
 					+ "Kicks from the channel and moves the client to the default channel.");
 		if (checkCommandPermission(sender, "set"))
 			send(sender, Level.INFO, "&e/tsa set (property) (value) &2- Change BukkitSpeak's config.");

18df5ef02ab89c6ea52251d6d75f66a377fa0074
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-05-20 01:43:21 +0200

    Removed old unused code.

--- src/main/java/net/but2002/minecraft/BukkitSpeak/teamspeakEvent/TeamspeakListener.java
+++ src/main/java/net/but2002/minecraft/BukkitSpeak/teamspeakEvent/TeamspeakListener.java
@@ -31,50 +31,5 @@ public class TeamspeakListener implements TeamspeakActionListener {
 		} else if (eventType.equals("notifyclientmoved")) {
 			new ClientMovedEvent(eventInfo);
 		}
-		
-		/*
-		String msg = eventInfo.remove("msg");
-		
-		//Add Team Speak Commands ingame commands.
-		if(msg.contains("!playerlist")){
-			Player[] pl = plugin.getServer().getOnlinePlayers();
-			if (stringManager.getTeamspeakTarget() == TsTargetEnum.CHANNEL) {
-				plugin.query.sendTextMessage(stringManager.getChannelID(),
-						JTS3ServerQuery.TEXTMESSAGE_TARGET_CHANNEL, "==Minecraft PlayerList==");
-			} else if (stringManager.getTeamspeakTarget() == TsTargetEnum.SERVER) {
-				plugin.query.sendTextMessage(0, JTS3ServerQuery.TEXTMESSAGE_TARGET_VIRTUALSERVER,
-						"==Minecraft PlayerList==");
-			}
-			for (int i=0; i<pl.length; i++){
-				String name = pl[i].getName();
-				String format = "[MC]" + name;
-				if (stringManager.getTeamspeakTarget() == TsTargetEnum.CHANNEL) {
-					plugin.query.sendTextMessage(stringManager.getChannelID(),
-							JTS3ServerQuery.TEXTMESSAGE_TARGET_CHANNEL, format);
-				} else if (stringManager.getTeamspeakTarget() == TsTargetEnum.SERVER) {
-					plugin.query.sendTextMessage(0, JTS3ServerQuery.TEXTMESSAGE_TARGET_VIRTUALSERVER, format);
-				}
-			}
-			return;
-		}
-		
-		//Example of usage
-		if(msg.contains("!reload")){
-			if(plugin.query.removeAllEvents()){
-			}
-			plugin.query.removeTeamspeakActionListener();
-			plugin.query.closeTS3Connection();
-			plugin.getServer().getPluginManager().disablePlugin(plugin);
-			plugin.getServer().getPluginManager().enablePlugin(plugin);
-			String format = "Example";
-			if (stringManager.getTeamspeakTarget() == TsTargetEnum.CHANNEL) {
-					plugin.query.sendTextMessage(stringManager.getChannelID(),
-							JTS3ServerQuery.TEXTMESSAGE_TARGET_CHANNEL, format);
-				} else if (stringManager.getTeamspeakTarget() == TsTargetEnum.SERVER) {
-					plugin.query.sendTextMessage(0, JTS3ServerQuery.TEXTMESSAGE_TARGET_VIRTUALSERVER, format);
-				}
-			return;
-		}
-		*/
 	}
 }

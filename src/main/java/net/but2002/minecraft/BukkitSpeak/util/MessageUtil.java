5e36f58c3d77e133a337de4956a0a2e42e6c6173
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-10-02 12:09:25 +0200

    Improved URL filtering. Replace new lines chars with a single space.

--- src/main/java/net/but2002/minecraft/BukkitSpeak/util/MessageUtil.java
+++ src/main/java/net/but2002/minecraft/BukkitSpeak/util/MessageUtil.java
@@ -94,9 +94,9 @@ public final class MessageUtil {
 		}
 		
 		if (links) {
-			s = s.replaceAll("(?i)((\\S*\\.)?[\\w\\-]+\\.[\\w\\-]+(/[^\\s\\[]*)?)", "\\[URL]$1\\[/URL]");
+			s = s.replaceAll("(?i)((\\S*)?[\\w\\-]+\\.[\\w\\-]+(/[^\\s\\[]*)?)", "\\[URL]$1\\[/URL]");
 		} else {
-			s = s.replaceAll("(?i)((\\S*\\.)?[\\w\\-]+\\.[\\w\\-]+(/[^\\s\\[]*)?)", "");
+			s = s.replaceAll("(?i)((\\S*)?[\\w\\-]+\\.[\\w\\-]+(/[^\\s\\[]*)?)", "");
 		}
 		
 		return s;
@@ -111,9 +111,9 @@ public final class MessageUtil {
 				s = s.replaceAll("((&|$|\u00A7)([a-fk-orA-FK-OR0-9]))", "");
 			}
 			if (links) {
-				s = s.replaceAll("(?i)(\\[URL])?((\\S*\\.)?[\\w\\-]+\\.[\\w\\-]+(/[^\\s\\[]*)?)(\\[/URL])?", "$2");
+				s = s.replaceAll("(?i)\\[URL]((\\S*)?[\\w\\-]+\\.[\\w\\-]+(/[^\\s\\[]*)?)\\[/URL]", "$2");
 			} else {
-				s = s.replaceAll("(?i)(\\[URL])?((\\S*\\.)?[\\w\\-]+\\.[\\w\\-]+(/[^\\s\\[]*)?)(\\[/URL])?", "");
+				s = s.replaceAll("(?i)(\\[URL])?((\\S*)?[\\w\\-]+\\.[\\w\\-]+(/[^\\s\\[]*)?)(\\[/URL])?", "");
 			}
 			return s;
 		}

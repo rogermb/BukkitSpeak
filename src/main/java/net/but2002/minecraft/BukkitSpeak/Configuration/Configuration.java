a004b1256a2c2ea3930a0581f69e2c30a4d0be80
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2014-07-05 14:45:31 +0200

    Change default ConsoleName color to red to improve visibility on TS.

--- src/main/java/net/but2002/minecraft/BukkitSpeak/Configuration/Configuration.java
+++ src/main/java/net/but2002/minecraft/BukkitSpeak/Configuration/Configuration.java
@@ -22,7 +22,7 @@ public enum Configuration {
 	MAIN_PASSWORD("main.QueryPassword", "123456"),
 
 	TS_NICKNAME("teamspeak.TeamspeakNickname", "Minecraft"),
-	TS_CONSOLE_NAME("teamspeak.ConsoleName", "&eServer"),
+	TS_CONSOLE_NAME("teamspeak.ConsoleName", "&4Server"),
 	TS_CHANNEL_ID("teamspeak.ChannelID", 0),
 	TS_CHANNEL_PASSWORD("teamspeak.ChannelPassword", ""),
 	TS_ENABLE_SERVER_EVENTS("teamspeak.SendServerEventsToMinecraft", true,

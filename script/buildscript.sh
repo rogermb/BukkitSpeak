4321259e0a9086c47abeda2013af9f8c52415029
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2015-03-22 03:23:37 +0100

    Double brackets for wildcards

--- script/buildscript.sh
+++ script/buildscript.sh
@@ -1,7 +1,7 @@
 version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
 echo "Project version: $version"
 
-if [ "$version" == *SNAPSHOT ]; then
+if [[ "$version" == *SNAPSHOT ]]; then
 	echo "Snapshot build, omitting javadoc"
 	mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
 else

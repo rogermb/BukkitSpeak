8f93daf04ccc5a37c7fb0cc159bf63244ca2c9b1
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2015-03-22 03:08:39 +0100

    Create buildscript.sh

---
+++ script/buildscript.sh
@@ -0,0 +1,10 @@
+version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+echo "Project version: $version"
+
+if [ "$version" == *SNAPSHOT ]; then
+	echo "Snapshot build, omitting javadoc"
+	mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+else
+	echo "Release build, generating javadoc"
+	mvn install -DskipTests=true -B -V
+fi
